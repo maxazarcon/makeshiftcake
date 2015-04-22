2a33d635ae288ee1f2b7ded5d791b5f58455a5d7
Max Azarcon <max@trinitysedge.com>
2015-04-22 18:49:58 -0400

    update essential.js

--- assets/js/essential.js
+++ assets/js/essential.js
@@ -7,10 +7,18 @@ $(document).ready(function(){
 // Load elements ------------------------------------------
 
 $(document).ready(function() {
+	
+	$("about").load("pages/about.html");
+	$("#contact").load("pages/contact.html");
+	$("#donate").load("pages/donate.html");
+	$("#home").load("pages/home.html");
+	$("#ranks").load("pages/ranks.html");
+	$("#rules").load("pages/rules.html");
+	$("#staff").load("pages/staff.html");
+	
 	$('a').click(function() {
 		var divname = this.name;
 
-		$("#" + divname).load("pages/" + divname + ".html");
 		$("#" + divname).show().siblings().hide();
 	});
 });
