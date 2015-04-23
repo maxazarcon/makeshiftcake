515a7f6dba1b6f92455a44031357c23d7c7b610d
Max Azarcon <max@trinitysedge.com>
2015-04-22 21:03:39 -0400

    plz werk

--- assets/js/essential.js
+++ assets/js/essential.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
 $("div").click(function() {
 	var divname = this.attr('id');
 
-	alert(this.attr("id"));
+	alert("click");
 	$("#" + divname + "Page").show("slow").siblings().hide("slow");
 });
 
